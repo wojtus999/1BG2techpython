aa9f141d5f0caadad56e5c6f8b5bd1c813a5d552
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-10-03 11:20:46 +0000

    zrobiono zadania 1-3 z karty pracy

---
+++ KartaPracy1.py
@@ -0,0 +1,16 @@
+# # zad 1
+
+# a = int(input())
+# b = int(input())
+# print(a ** 2 + b ** 2
+
+# Zad 2
+
+# a = int(imput())
+# b = int(imput())
+# print((a+b) ** 2)
+
+# Zad 3
+# a, b = int(input()), int(input())
+# print((a - b) **3)
+
