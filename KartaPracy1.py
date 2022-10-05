4bf559027ca1d8a130ae890eff93004bc675ce5a
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-10-05 10:55:50 +0000

    skończone

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,16 +1,21 @@
-# # zad 1
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
 
 # a = int(input())
 # b = int(input())
-# print(a ** 2 + b ** 2
+# print((a+b)**2)
 
-# Zad 2
-
-# a = int(imput())
-# b = int(imput())
-# print((a+b) ** 2)
-
-# Zad 3
 # a, b = int(input()), int(input())
-# print((a - b) **3)
+# print((a-b)**3)
 
+# a = int(input())
+# b = int(imput())
+# print = (2*(a+b)/5)
+
+# brutto = int(input())
+# print = (brutto/ 1,23)
+
+# a = int(input())
+# b = int(input())
+# print(a%b)
