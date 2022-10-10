4563830fd9408241ed27877769caf77d7d404b5f
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-10-10 11:19:15 +0000

    dokończono karte pracy numer 2.py + ankieta

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -22,3 +22,31 @@
 #   print("A jedź")
 # else:
 #   print("Bezapelacyjny STOP")
+
+# a = int(input("Podaj liczbe:"))
+# b = int(input("Podaj liczbę:"))
+# if a > b:
+#   print ("TAK")
+# else:
+#   print ("NIE")
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a < c < b or b > c > a:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# a = int(input())
+# p = int(input())
+# if (a**p - a) % p == 0:
+#   print("TAK spełnia MTF")
+# else:
+#   print("NIE mie spełnia MTF")
+
+# p, k, s = map(int, input().split())
+# if p + 3*s >= k:
+#   print("TAK, płaz przeskoczy")
+# else: 
+#   print("NIE, żaba nie da rady")
