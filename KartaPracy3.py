4968ff9c51bdb26107081fc44eb4882fc87388dc
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-10-12 11:20:32 +0000

    robiłem pętle

---
+++ KartaPracy3.py
@@ -0,0 +1,21 @@
+# for i in range(100):
+#   print(i, end=" ")
+
+# for i in range(10, 22): print(i, end=" ")
+
+# for i in range(15,33,2): print(i, end=" ")
+
+# for i in range(99,9,-1): print(i, end=" ")
+
+# for i in range(-99,-9,1): print(i*(-1), end=" ")
+
+# for i in range(100, 1000, 20): print(i, end=" ")
+
+#Zad 1
+# n = int(input())
+# for i in range(n):
+#   print(i**3 + 3, end=" ")
+
+#Zad 2
+for i in range(105, 1000, 15) :
+  print(i, end=" ")
