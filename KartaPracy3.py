e8bad05618277b460ace2914fe282712808c3a52
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-10-19 11:20:25 +0000

    zrobiłem 1 zdanie + kartkówka

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -19,3 +19,34 @@
 #Zad 2
 # for i in range(105, 1000, 15) :
 #   print(i, end=" ")
+
+# PRE 2
+# Pętlę for liczb trzycyfrowych podzielnych przez 13
+# for i in range(100, 1000):
+#   if i% 13 ==0:
+#     print(i, end=" ")
+# Pętlę for ilczb liczb dwucyfrowych parzystych
+# for i in range(10, 100, 2): print(i, end=" ")
+# Pętlę for potęg cyfr: 0, 1, 4, 9, 16 ... 81
+# for i in range(10): print(i*i, end=" ")
+
+# Zad 3
+# n = int(input())
+# for i in range(1, n+1):
+#   if n % i == 0:
+#     print(i)
+# Zad 4
+# suma = 0
+# for i in range(10, 100):
+#   suma = suma + i
+# print(suma)
+
+#DOD - suma liczb trzycyfrowych parzystych
+# suma = 0
+# for i in range(100, 1000, 2):
+#   suma = suma + i
+# print(suma)\
+
+# Zad 5
+liczba = 0
+a = int(input("Podaj liczbe graczy: "))
---
+++ kartkowka.py
@@ -0,0 +1,4 @@
+# Zad 4
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+if a > b > c > d or d > a > b > c or c > d > a > b or b > c > d > a:
+  print("Najmniejsza liczba to")
