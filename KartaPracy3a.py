b3d1294920b356dc1af52a7342e812e7596396e1
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-10-26 11:19:34 +0000

    zrobiłem 2 zadania

---
+++ KartaPracy3a.py
@@ -0,0 +1,19 @@
+# Zad 1
+# n = int(input())
+# for i in range(n):
+#   print("*-|", end=" ")
+
+# PRE 1
+# print(5+6)
+# print("Bartosz"+"Nowak")
+# print("Bartosz"+5)
+# print("Bartosz"*5)
+
+# Zad 2
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*"*i, end=" ")
+#   if i % 2 ==1:
+#     print("||", end=" ")
+#   else:
+#     print("--", end=" ")
