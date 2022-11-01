91d8e2befbe333366f425fac842568098641af9a
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-11-01 20:11:36 +0000

    Zrobiłem wszystko oprócz 7

---
+++ KartaPracy3b.py
@@ -0,0 +1,75 @@
+# Zad 1 
+# for i in range(1,31):
+#     print(i, "dni")
+
+# Zad 2
+
+# for i in range(1, 10, 2):
+#     print(i**2, "no kwadraty ma sie rozumieć")
+
+# Zad 3
+
+# for i in range(1138, 9855):
+#     if i %379 ==0:
+#         print(i)
+
+# Zad 4
+
+# for i in range(101, 1000):
+#     if i %5 ==0 and i %6 ==0 and i %11 ==0:
+#         print(i)
+
+# Zad 5
+
+# a = int(input("podaj ilośc liczb: "))
+# Liczby = []
+# for i in range(0, a):
+#     temp = int(input(f"Podaj {i+1} liczbę: "))
+#     Liczby.append(temp)
+# print(f"Wynik to: {sum(Liczby)}")
+
+# Zad 6
+
+# k = int(input("podaj ilośc liczb: "))
+# Liczby = []
+# for i in range(k,100,2):
+#     if k %2 ==0:
+#         print("tak")
+#     else:
+#         print("nie")
+
+# Zad 7
+
+
+
+
+
+
+
+
+
+# Zad 8
+
+# w0 = int(input("podaj kwote wejściową: "))
+# l = int(input("podaj okres inwestycji(w miesiącach): "))
+# wk = 0
+# suma = wk
+# for i in range(0, l * 0,5):
+#     suma = suma + wk
+#     print(suma)
+
+# Zad 9
+#skończ
+# n = int(input())
+# for i in range(0, n):
+#     if n %3 ==0 and n %7 ==0:
+#         print("tak")
+
+# Zad 10
+
+ # import math
+# for i in range(1,1000):
+#   if i%10==math.sqrt(i):
+#     print(i)
+#   elif i%100==math.sqrt(i):
+#     print(i)
