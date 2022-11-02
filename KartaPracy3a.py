6b8f0d4135a0b3c6525acdb990c51f5685ad3aa9
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-11-02 12:20:41 +0000

    robiłem zadania

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -17,3 +17,12 @@
 #     print("||", end=" ")
 #   else:
 #     print("--", end=" ")
+
+# Zad 3
+n = int(input())
+for i in range(1, n+1):
+  print("*", end=" ")
+  if i % 2 == 0:
+    print("-"*i, end=" ")
+  else:
+    print("|"*i, end=" ")
