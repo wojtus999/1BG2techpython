488f416d167ed049aa04ed331a4b4beebc8d62d2
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-11-14 12:21:13 +0000

    coś tam robiłem

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -19,10 +19,15 @@
 #     print("--", end=" ")
 
 # Zad 3
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*", end=" ")
+#   if i % 2 == 0:
+#     print("-"*i, end=" ")
+#   else:
+#     print("|"*i, end=" ")
+
+# Zad 4
 n = int(input())
-for i in range(1, n+1):
-  print("*", end=" ")
-  if i % 2 == 0:
-    print("-"*i, end=" ")
-  else:
-    print("|"*i, end=" ")
+for i in range(n):
+  print("*")
---
+++ SPRAWDZIAN.py
@@ -0,0 +1,25 @@
+# Zad 1
+
+# p, k, s = map(int, input().split())
+# if p + 3*s >= k:
+#   print("TAK, dojdzie dokońca")
+# else:
+#   print("Niestety nie dojdzie do końca :(")
+
+#Zad 2
+
+#Nie wiem
+
+#Zad 3
+
+# n = int(input("Podaj liczbę trzycyfrową"))
+# if n % 6 and 8 == 0:
+#   print("Liczba ta dzieli się przez 6 lub 8 bądź jest nieparzysta")
+    
+#Zad 4
+
+# suma = 0
+# for i in range(1000, 10000):
+#   if i % 483 == 0:
+#     suma = suma + i
+#     print(suma)
