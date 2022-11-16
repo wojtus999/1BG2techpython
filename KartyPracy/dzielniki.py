c6ef9d99215e04bd526ea02c7ba7b693a09d3613
1B27WS <114579340+wojtus999@users.noreply.github.com>
2022-11-16 12:20:53 +0000

    poprawiałem + zrobiłem folder carti to goat

---
+++ KartyPracy/dzielniki.py
@@ -0,0 +1,11 @@
+# n = int(input())
+# licznik = 0
+# for i in range(1, n+1):
+#   if n % i == 0:
+#     licznik += 1
+
+# if licznik == 0:
+#   print("Liczba jest pierwsza")
+# else:
+#   print("Liczba nie jest pierwsza")
+
---
+++ KartyPracy/nowe.py
@@ -0,0 +1,2 @@
+# a, b, c, d = int(input("Podaj a: ")), int(input("Podaj b: ")), int(input("Podaj c: ")), int(input("Podaj d: "))
+# print("Najmniejsza z podanych liczb to: ", min(a, b, c, d))
