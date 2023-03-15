788bf09afd0887853123ccfdd534f1fbe7d8baa0
1B27WS <114579340+wojtus999@users.noreply.github.com>
2023-03-15 12:19:37 +0000

    sortowanie

---
+++ Sortowanie/przezWybor.py
@@ -0,0 +1,14 @@
+# Wygeneruj listę 10 losowych licz z przedziału [1, 30]
+
+from random import randint
+n = 10
+L = [randint(1, 30) for i in range(n)]
+print(L)
+
+for i in range(n):
+     mi = i
+  for j in range(i+1, n):
+      if L[j] < L[mi]:
+           mi = j
+      L[i], L[mi] = L[mi], L[i]
+print(L)
